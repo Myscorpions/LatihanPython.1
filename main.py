983bc663c58e61f1542fe50e198e6b22a7c089a8
Scorpions <87361010+yusufimanuel@users.noreply.github.com>
2021-09-13 16:19:03 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+print("\nPengubah Suhu\n")
+
+celcius = float(input('Masukan Suhu Dalam Celsius : '))
+print("suhu adalah",celcius, "Celsius")
+
+reamur = (4/5) * celcius
+print("suhu dalama reamur adalah ",reamur, "Reamur")
+
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah ",fahrenheit, "Fahrenheit")
+
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah ",kelvin, "Kelvin")
